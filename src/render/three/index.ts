c53d1519a5993ce1bf0464c59dd6ef941b1b3ec5
Hunter Caron <huntercaron@me.com>
2020-06-18 15:52:17 +0200

    Setting up three rendering files

--- src/render/three/index.ts
+++ src/render/three/index.ts
@@ -1,29 +1,51 @@
 // import * as THREE from "three"
-// import { createMotionComponent } from "../../motion"
-// import { createThreeMotionConfig } from "./create-three-config"
+import { createMotionComponent, MotionComponentConfig } from "../../motion"
+import { VisualElement } from "../VisualElement"
+import { useConstant } from "../../utils/use-constant"
+import { render } from "./render"
 
-// export const motion = Object.keys(THREE).reduce((components, key) => {
-//     const lowercaseKey = key[0].toLowerCase() + key.substring(1)
-//     components[lowercaseKey] = createMotionComponent(
-//         createThreeMotionConfig(lowercaseKey)
-//     )
-//     return components
-// }, {})
+// @ts-nocheck
+// @ts-ignore
+class ThreeVisualElement extends VisualElement {
+    // getBoundingBox() {}
+    render() {
+        this.element.position = [this.latest.x, this.latest.y, this.latest.z]
+    }
+}
 
-// const config: MotionComponentConfig<HTMLElement | SVGElement> = {
-//     useVisualElement: useThreeVisualElement,
-//     render: render,
-//     animationControlsConfig: {},
-// }
+export const useThreeVisualElement = (
+    // Component: any,
+    props: any,
+    parent: any,
+    // isStatic: any,
+    ref: any
+) => {
+    const visualElement = useConstant(
+        () => new ThreeVisualElement(parent, ref as any)
+    )
 
-// // This just caches generated components
-// const componentCache = new Map<string, any>()
+    visualElement.updateConfig(props)
 
-// export const motion = new Proxy({}, {
-//   get: (target, key) {
-//     if (!componentCache.has(key)) {
-//         componentCache.set(key, createMotionComponent(key, config))
-//     }
-//     return componentCache.get(key)
-// }
-// })
+    return visualElement
+}
+
+const config: MotionComponentConfig<HTMLElement | SVGElement> = {
+    useVisualElement: useThreeVisualElement as any,
+    render: render as any,
+    animationControlsConfig: {},
+}
+
+// This just caches generated components
+const componentCache = new Map<string, any>()
+
+export const motion = new Proxy(
+    {},
+    {
+        get: (target, key: any) => {
+            if (!componentCache.has(key)) {
+                componentCache.set(key, createMotionComponent(key, config))
+            }
+            return componentCache.get(key)
+        },
+    }
+)
---
+++ src/render/three/render.ts
@@ -0,0 +1,39 @@
+import { createElement } from "react"
+import { MotionProps } from "../../motion/types"
+import { filterProps } from "../dom/utils/filter-props"
+
+export function buildThreeProps(visualElement: any) {
+    return {
+        ...visualElement.attrs,
+        style: { ...visualElement.reactStyle },
+    }
+}
+
+export function render<Props>(
+    Component: string | React.ComponentType<Props>,
+    props: MotionProps,
+    visualElement: any
+) {
+    // Only filter props from components we control, ie `motion.div`. If this
+    // is a custom component pass along everything provided to it.
+    const forwardedProps =
+        typeof Component === "string" ? filterProps(props) : props
+
+    /**
+     * Every render, empty and rebuild the animated values to be applied to our Element.
+     * During animation these data structures are used in a mutable fashion to reduce
+     * garbage collection, but between renders we can flush them to remove values
+     * that might have been taken out of the provided props.
+     */
+    visualElement.clean()
+    visualElement.build()
+
+    // Generate props to visually render this component
+    const visualProps = buildThreeProps(visualElement as any)
+
+    return createElement<any>(Component, {
+        ...forwardedProps,
+        ref: visualElement.ref,
+        ...visualProps,
+    })
+}
