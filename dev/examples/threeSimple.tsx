365c1fdfc8282641c7f1f10486acebf7173b3d04
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-06-19 14:30:58 +0200

    Add narrative keyboard shortcuts

--- dev/examples/threeSimple.tsx
+++ dev/examples/threeSimple.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import * as THREE from "three"
-import { useCallback, useState } from "react"
+import { useCallback, useState, useEffect, Suspense } from "react"
 import { motion } from "../../src/render/three"
 import { AnimatePresence } from "@framer"
 import { Canvas } from "react-three-fiber"
 import { OrbitControls } from "drei"
+import { Asset } from "./framerLogo"
 
 const GOLDEN_RATIO = (Math.sqrt(5) + 1) / 2 - 1
 const GOLDEN_ANGLE = GOLDEN_RATIO * (2 * Math.PI)
@@ -19,12 +20,10 @@ const primitives = [
     <torusKnotBufferGeometry attach="geometry" args={[0.1, 0.02, 26, 12]} />,
 ]
 
-const getRandomPrimitive = () => {
-    return primitives[Math.floor(Math.random() * primitives.length)]
-}
-
 export const App = () => {
-    const [meshes, setMeshes] = useState([])
+    const [meshes, setMeshes] = useState(Array.from(Array(40).keys()))
+    const [showMeshes, setShowMeshes] = useState(false)
+    const [hover, setHover] = useState(false)
     const [orbit, setOrbit] = useState(false)
 
     const handleAdd = useCallback(() => {
@@ -40,17 +39,31 @@ export const App = () => {
         })
     }, [])
 
-    const handleRemove = useCallback(() => {
-        setMeshes(latestMeshes => {
-            latestMeshes.shift()
+    const handleKey = useCallback((event: KeyboardEvent) => {
+        if (event.key === "q") {
+            setShowMeshes(latestShowMeshes => !latestShowMeshes)
+        }
 
-            return [...latestMeshes]
-        })
+        if (event.key === "w") {
+            setMeshes(latestMeshes => {
+                latestMeshes.shift()
+
+                return [...latestMeshes]
+            })
+        }
+
+        if (event.key === "e") {
+            setOrbit(latestOrbit => !latestOrbit)
+        }
     }, [])
 
-    const handleOrbit = useCallback(() => {
-        setOrbit(latestOrbit => !latestOrbit)
-    }, [])
+    useEffect(() => {
+        window.addEventListener("keydown", handleKey)
+
+        return () => {
+            window.removeEventListener("keydown", handleKey)
+        }
+    }, [handleKey])
 
     return (
         <Canvas
@@ -80,7 +93,13 @@ export const App = () => {
                     <pointLight position={[100, 100, 100]} intensity={2} />
                 </>
             )}
-            <motion.mesh initial={{ x: 0 }} onClick={handleAdd}>
+            <motion.mesh
+                initial={{ x: 0, scale: 1 }}
+                animate={{ x: 0, scale: hover ? 1.3 : 1 }}
+                onClick={handleAdd}
+                onPointerOver={() => setHover(true)}
+                onPointerOut={() => setHover(false)}
+            >
                 {orbit ? (
                     <meshPhongMaterial attach="material" />
                 ) : (
@@ -88,57 +107,54 @@ export const App = () => {
                 )}
                 <sphereBufferGeometry attach="geometry" args={[0.6, 16, 16]} />
             </motion.mesh>
-            <motion.mesh initial={{ x: 1 }} onClick={handleRemove}>
-                {orbit ? (
-                    <meshPhongMaterial attach="material" />
-                ) : (
-                    <meshBasicMaterial attach="material" />
-                )}
-                <sphereBufferGeometry attach="geometry" args={[0.2, 16, 16]} />
-            </motion.mesh>
-            <motion.mesh initial={{ x: -1 }} onClick={handleOrbit}>
-                {orbit ? (
-                    <meshPhongMaterial attach="material" />
-                ) : (
-                    <meshBasicMaterial attach="material" />
-                )}
-                <sphereBufferGeometry attach="geometry" args={[0.2, 16, 16]} />
-            </motion.mesh>
             <AnimatePresence>
-                {meshes.map((mesh, index) => {
-                    const ratio = (index + 40) / 200
-                    const angle = (index + 40) * GOLDEN_ANGLE
-                    const rad = ratio * -8
-                    const x = Math.cos(angle) * rad
-                    const y = Math.sin(angle) * rad
-                    const z = index * -0.1
+                {showMeshes &&
+                    meshes.map((mesh, index) => {
+                        const ratio = (index + 40) / 200
+                        const angle = (index + 40) * GOLDEN_ANGLE
+                        const rad = ratio * -8
+                        const x = Math.cos(angle) * rad
+                        const y = Math.sin(angle) * rad
+                        const z = index * -0.1
 
-                    return (
-                        <motion.mesh
-                            key={mesh}
-                            initial={{ opacity: 0, x: 0, y: 0, z: 0, scale: 0 }}
-                            animate={{ opacity: 1, x, y, z, scale: 1 }}
-                            exit={{ opacity: 0, x: 0, y: 0, z: 0, scale: 0 }}
-                            transition={{
-                                duration: 1,
-                            }}
-                        >
-                            {orbit ? (
-                                <meshPhongMaterial attach="material" />
-                            ) : (
-                                <meshBasicMaterial attach="material" />
-                            )}
-                            {orbit ? (
-                                getRandomPrimitive()
-                            ) : (
-                                <sphereBufferGeometry
-                                    attach="geometry"
-                                    args={[0.1, 12, 12]}
-                                />
-                            )}
-                        </motion.mesh>
-                    )
-                })}
+                        return (
+                            <motion.mesh
+                                key={mesh}
+                                initial={{
+                                    opacity: 0,
+                                    x: 0,
+                                    y: 0,
+                                    z: 0,
+                                    scale: 0,
+                                }}
+                                animate={{ opacity: 1, x, y, z, scale: 1 }}
+                                exit={{
+                                    opacity: 0,
+                                    x: 0,
+                                    y: 0,
+                                    z: 0,
+                                    scale: 0,
+                                }}
+                                transition={{
+                                    duration: 1,
+                                }}
+                            >
+                                {orbit ? (
+                                    <meshPhongMaterial attach="material" />
+                                ) : (
+                                    <meshBasicMaterial attach="material" />
+                                )}
+                                {orbit ? (
+                                    primitives[index % primitives.length]
+                                ) : (
+                                    <sphereBufferGeometry
+                                        attach="geometry"
+                                        args={[0.1, 12, 12]}
+                                    />
+                                )}
+                            </motion.mesh>
+                        )
+                    })}
             </AnimatePresence>
             {orbit && <OrbitControls />}
         </Canvas>
